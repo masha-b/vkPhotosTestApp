5d769e36d23558ad3f5d95a2a05a6f53a96c9028
Мария Бирюкова <maria@sitesoft.ru>
2016-08-30 16:51:10 +0500

    Initial Commit

---
+++ myPhotos/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  myPhotos
+//
+//  Created by Мария on 30.08.16.
+//  Copyright © 2016 Мария. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
